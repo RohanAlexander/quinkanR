598766e2ffb5f23961ffa91bf91b2fdc2ae2834c
Rohan Alexander <rohan.p.alexander@gmail.com>
2017-04-23 18:46:40 +1000

    Upload data
    Initial upload of the NSW colonial census data.

---
+++ R/nsw_county_population.R
@@ -0,0 +1,12 @@
+#' Historical NSW census data
+#'
+#' A dataset containing the population of historical counties in NSW
+#'
+#' @format A data frame with 47675 rows and 8 variables:
+#' \describe{
+#'   \item{year}{The census year of the data}
+#'   \item{area_type}{Is the area a county or a pastoral district?}
+#'   ...
+#' }
+#' @source \url{http://hccda.ada.edu.au/}
+"nsw_county_population"
